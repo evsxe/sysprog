d62693972efce38ec69ea22c092a462ea5ef46ae
Салуев Евгений Алексеевич <easaluev@edu.hse.ru>
2024-03-24 22:36:00 +0300

    Added a new solution task

--- 1/solution.c
+++ 1/solution.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <limits.h>
 #include "libcoro.h"
+#include <limits.h>
+#include <time.h>
 
-struct my_context {
-    char *name;
-    char **file_list;
-    int file_count;
-    int *file_idx;
-    int **arr_p;
-    int *arr;
-    int *size_p;
+
+struct
+my_context
+{
+    char *coro_name;
+    int f_count;
+    char **file_names;
+    int *f_index;
+    int **data;
+    int *array;
+    int *array_size;
+
     int sec_start;
     int nsec_start;
     int sec_finish;
@@ -21,43 +25,72 @@ struct my_context {
     int nsec_total;
 };
 
-// Allocate and initialize the context object
-static struct my_context *my_context_new(const char *name, char **file_list, int file_count,
-                                         int *idx, int **data_p, int *size_p) {
-    struct my_context *ctx = malloc(sizeof(*ctx));
-    ctx->name = strdup(name);
-    ctx->file_list = file_list;
-    ctx->file_idx = idx;
-    ctx->file_count = file_count;
-    ctx->arr_p = data_p;
-    ctx->size_p = size_p;
-    return ctx;
+
+int
+calc_nums_in_file(FILE *file)
+{
+    int count = 0;
+    int numbers;
+
+    rewind(file);
+    while (fscanf(file, "%d", &numbers) == 1) {
+        count++;
+    }
+
+    rewind(file);
+    return count;
 }
 
-// Deallocate the context and its fields
+void fill_array(FILE *file, int *array) {
+    int i = 0;
+
+    rewind(file);
+    while (fscanf(file, "%d", &array[i]) == 1) {
+        i++;
+    }
+
+    rewind(file);
+}
+
+
+
+static struct my_context *
+my_context_new(char *coro_name, char **file_names, int f_count,
+               int *f_index, int **data, int *array_size)
+{
+    struct my_context *ctx = malloc(sizeof(*ctx));
+    ctx->coro_name = strdup(coro_name);
+    ctx->file_names = file_names;
+    ctx->f_index = f_index;
+    ctx->f_count = f_count;
+    ctx->data = data;
+    ctx->array_size = array_size;
+    return ctx;
+
+}
+
 static void my_context_delete(struct my_context *ctx) {
-    free(ctx->name);
+    free(ctx->coro_name);
     free(ctx);
 }
 
-// Stop the timer and update the finish time in the context
-static void stop_timer(struct my_context *ctx) {
+static void stop_time(struct my_context *ctx) {
     struct timespec time;
     clock_gettime(CLOCK_MONOTONIC, &time);
     ctx->sec_finish = time.tv_sec;
     ctx->nsec_finish = time.tv_nsec;
 }
 
-// Start the timer and update the start time in the context
-static void start_timer(struct my_context *ctx) {
+
+static void start_time(struct my_context *ctx) {
     struct timespec time;
     clock_gettime(CLOCK_MONOTONIC, &time);
     ctx->sec_start = time.tv_sec;
     ctx->nsec_start = time.tv_nsec;
 }
 
-// Calculate the total context time, considering nanoseconds overflow
 static void calculate_time(struct my_context *ctx) {
+
     long long diff_sec = ctx->sec_finish - ctx->sec_start;
     long long diff_nsec = ctx->nsec_finish - ctx->nsec_start;
 
@@ -70,182 +103,174 @@ static void calculate_time(struct my_context *ctx) {
     ctx->nsec_total += diff_nsec;
 }
 
-// Swap two integers
-static void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void concat(int *arr, int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = malloc(sizeof(int) * n1);
+    int *R = malloc(sizeof(int) * n2);
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
 }
 
-// Perform quicksort on the array using coroutines without a separate partition function
-void quick_sort(int *array, int left, int right, struct my_context *ctx) {
-    int stack[right - left + 1];
-    int top = -1;
+void mergeSort(int *array, int l, int r, struct my_context *ctx) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
 
-    stack[++top] = left;
-    stack[++top] = right;
+        mergeSort(array, l, m, ctx);
+        mergeSort(array, m + 1, r, ctx);
 
-    while (top >= 0) {
-        right = stack[top--];
-        left = stack[top--];
+        concat(array, l, m, r);
 
-        int pivot = array[right];
-        int i = left;
-
-        for (int j = left; j < right; j++) {
-            if (array[j] <= pivot) {
-                swap(&array[i], &array[j]);
-                i++;
-            }
-        }
-
-        swap(&array[i], &array[right]);
-        int pi = i;
-
-        if (pi - 1 > left) {
-            stack[++top] = left;
-            stack[++top] = pi - 1;
-        }
-
-        if (pi + 1 < right) {
-            stack[++top] = pi + 1;
-            stack[++top] = right;
-        }
-
-        stop_timer(ctx);
+        stop_time(ctx);
         calculate_time(ctx);
         coro_yield();
-        start_timer(ctx);
+        start_time(ctx);
     }
 }
 
-// Coroutine function
-static int coroutine_func_f(void *context) {
+static int coro_func(void *context)
+{
     struct coro *this = coro_this();
-    struct my_context *ctx = (struct my_context *) context;
-    start_timer(ctx);
+    struct my_context *ctx = context;
 
-    while (*(ctx->file_idx) != ctx->file_count) {
-        char *filename = ctx->file_list[*(ctx->file_idx)];
-        FILE *in = fopen(filename, "r");
-        if (!in) {
+    start_time(ctx);
+    while (*(ctx->f_index) != ctx->f_count) {
+
+        int i = *ctx->f_index;
+        char *f_name = ctx->file_names[i];
+
+        printf("Coroutine %s sorting file %s...\n", ctx->coro_name, f_name);
+
+        FILE* file = fopen(f_name, "r");
+        if (!file) {
             my_context_delete(ctx);
-            return 1;
-        }
-        int size = 0;
-        int cap = 100;
-        ctx->arr = (int *) malloc(cap * sizeof(int));
-
-        // Read data from the text file
-        // Reallocate the array if the capacity has run out
-        while (fscanf(in, "%d", ctx->arr + size) == 1) {
-            ++size;
-            if (size == cap) {
-                cap *= 2;
-                ctx->arr = (int *) realloc(ctx->arr, cap * sizeof(int));
-            }
+            return 0;
         }
 
-        // Shrink the array to fit
-        cap = size;
-        ctx->arr = (int *) realloc(ctx->arr, cap * sizeof(int));
+        int count_of_numbers = calc_nums_in_file(file);
 
-        fclose(in);
+        ctx->array = (int *) malloc(count_of_numbers * sizeof(int ));
+        fill_array(file, ctx->array);
 
-        // Store the address of the allocated array and its size
-        ctx->arr_p[*(ctx->file_idx)] = ctx->arr;
-        ctx->size_p[*(ctx->file_idx)] = size;
+        fclose(file);
+        ctx->data[*(ctx->f_index)] = ctx->array;
+        ctx->array_size[*(ctx->f_index)] = count_of_numbers;
 
-        // Move to the next file
-        (*(ctx->file_idx))++;
+        (*(ctx->f_index))++;
+        mergeSort(ctx->array, 0, count_of_numbers - 1, ctx);
 
-        quick_sort(ctx->arr, 0, size - 1, ctx);
     }
 
-    stop_timer(ctx);
+    stop_time(ctx);
     calculate_time(ctx);
-
-    printf("%s info:\nswitch count %lld\nworked %d us\n\n",
-           ctx->name,
-           coro_switch_count(this),
-           ctx->sec_total * 1000000 + ctx->nsec_total / 1000
-    );
+    printf("%s: количество переключений - %lld, время выполнения: %d\n", ctx->coro_name, coro_switch_count(this),
+           ctx->sec_total * 1000000  + ctx->nsec_total / 1000);
 
     my_context_delete(ctx);
     return 0;
 }
 
-// Merge the sorted arrays
-static int merge(int **data, int *size, int *idx, int cnt) {
-    int min_idx = -1;
-    int curr_min = INT_MAX;
 
-    for (int i = 0; i < cnt; ++i) {
-        if (size[i] > idx[i] && data[i][idx[i]] < curr_min) {
-            curr_min = data[i][idx[i]];
-            min_idx = i;
-        }
-    }
+int
+main(int argc, char **argv)
+{
+    struct timespec start, end;
+    int time;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
-    return min_idx;
-}
+    int count_files = argc - 2;
+    int N = atoi(argv[1]);
+    int f_indx = 0;
 
-int main(int argc, char **argv) {
-    struct timespec start_time;
-    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    int *data[count_files];
+    int sizes[count_files];
 
     coro_sched_init();
 
-    int file_count = argc - 2;
-    int coroutine_count = atoi(argv[1]);
-
-    if (!coroutine_count || !file_count) {
-        fprintf(stderr, "Incorrect input format.\n"
-                        "\tExample: N test1.txt test2.txt test3.txt test4.txt test5.txt test6.txt\n");
-        return 1;
-    }
-
-    int *p[file_count];
-    int s[file_count];
-    int file_idx = 0;
-
-    for (int i = 0; i < coroutine_count; ++i) {
+    for (int i = 0; i < N; i++) {
         char name[16];
-        snprintf(name, sizeof(name), "coro_%d", i);
-        coro_new(coroutine_func_f,
-                 my_context_new(name, argv + 2, file_count, &file_idx, p, s));
+        sprintf(name, "coro_%d", i);
+
+        coro_new(coro_func,
+                 my_context_new(name, argv + 2, count_files, &f_indx, data, sizes));
+
     }
+
     struct coro *c;
     while ((c = coro_sched_wait()) != NULL) {
-        coro_delete(c);
+        if (c != NULL){
+            printf("Finished with status %d\n", coro_status(c));
+            coro_delete(c);
+        }
     }
 
-    int idx[file_count]; // Array of current indexes for merging arrays
-    for (int i = 0; i < file_count; ++i) {
-        idx[i] = 0;
-    }
+    int *indx = (int *) calloc(count_files, sizeof(int ));
 
     FILE *out = fopen("out.txt", "w");
 
     int min_idx = 0;
     while (min_idx != -1) {
-        min_idx = merge(p, s, idx, file_count);
+
+        int curr_min = INT_MAX;
+        min_idx = -1;
+        for (int i = 0; i < count_files; ++i) {
+            if (sizes[i] > indx[i] && data[i][indx[i]] < curr_min) {
+                curr_min = data[i][indx[i]];
+                min_idx = i;
+            }
+        }
         if (min_idx != -1) {
-            fprintf(out, "%d ", p[min_idx][idx[min_idx]]);
-            idx[min_idx] += 1;
+            fprintf(out, "%d ", data[min_idx][indx[min_idx]]);
+            indx[min_idx] += 1;
         }
     }
+
     fclose(out);
 
-    for (int i = 0; i < file_count; ++i) {
-        free(p[i]);
+    for (int i = 0; i < count_files; ++i) {
+        free(data[i]);
     }
 
-    struct timespec finish_time;
-    clock_gettime(CLOCK_MONOTONIC, &finish_time);
+    free(indx);
 
-    printf("total time: %ld us\n",
-           (finish_time.tv_sec - start_time.tv_sec) * 1000000 + (finish_time.tv_nsec - start_time.tv_nsec) / 1000);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 
+    printf("Время выполнения программы: %d\n", time);
     return 0;
 }
